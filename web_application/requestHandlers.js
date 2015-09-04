7c292b5b3cd1dff17452be388fd02589ee0677a2
Elisha Tam <elisha.tam@gmail.com>
2015-09-04 13:44:34 -0700

    Clean up /upload output so only displays the inputted text
    Use querystring

--- web_application/requestHandlers.js
+++ web_application/requestHandlers.js
@@ -1,3 +1,5 @@
+var querystring = require("querystring");
+
 //This is a request handler
 function start(response, postData) {
   console.log("Request handler 'start' was called.");
@@ -26,7 +28,8 @@ function start(response, postData) {
 function upload(response, postData) {
   console.log("Request handler 'upload' was called.");
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("You've sent: " + postData);
+  response.write("You've sent the text: " + 
+  querystring.parse(postData).text);
   response.end();
 }
 
